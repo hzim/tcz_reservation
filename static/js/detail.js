11deca6ff62f744e0e2c0199c6a0fd9a1c85941d
Heinrich Zimmerl <heinrich.zimmerl@gmail.com>
2018-01-07 10:30:03 +0100

    media query for smaller devices css style sheet alphabetical

--- static/js/detail.js
+++ static/js/detail.js
@@ -4,34 +4,38 @@ var dateInput = {
     var date_input = document.getElementById("id_req_date");
     var date_button = document.getElementById("id_SetDate");
 
-    date_input.onchange = function () {
-      date_button.click();
-
+    if (date_input != null) {
+      date_input.onchange = function () {
+        date_button.click();
+      }
     }
   }
 }
 
 var processChoices = {
   init: function () {
-    var selectedUser = document.getElementById("id_selectedUser");
-
-    selectedUser.onchange = function () {
-      //alert( selectedUser.find("option:selected").text());
-      document.getElementById("id_SetUser").click();
+    var selected_user = document.getElementById("id_selectedUser");
+    if (selected_user != null) {
+      selected_user.onchange = function () {
+        //alert( selected_user.find("option:selected").text());
+        document.getElementById("id_SetUser").click();
+      }
     };
 
     var navSave = document.getElementById("id_NavSave");
     var allSave = document.getElementById("id_AllSave");
 
     // if javascript is working enable the navigation bar save button and hide the form button
-    navSave.style.display = "initial";
-    allSave.style.display = "none";
+    if (navSave != null && allSave != null) {
+      navSave.style.display = "initial";
+      allSave.style.display = "none";
 
-    // if the navigation button is clicked fire the form button
-    navSave.onclick = function (event) {
-      event.preventDefault();
-      //alert( allSave );
-      allSave.click();
+      // if the navigation button is clicked fire the form button
+      navSave.onclick = function (event) {
+        event.preventDefault();
+        //alert( allSave );
+        allSave.click();
+      }
     };
 
   }
